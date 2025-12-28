add96dda7c8ff4d3e10a7b672e71b6d69c398cfc
Phil Stewart <p.stewart@imperial.ac.uk>
2025-12-28 14:26:19 +0000

    tzx/config: Replace buffer_size with buffer_length_ms; add playback_duration_percent

--- src/tzx/config.rs
+++ src/tzx/config.rs
@@ -10,12 +10,16 @@ pub struct Config {
     pub platform: Platform,
     #[builder(default = 44100 as SampleRate)]
     pub sample_rate: SampleRate,
-    #[builder(default = 1024)]
-    pub buffer_size: u32,
+    #[builder(default = 200)]
+    pub buffer_length_ms: u32,
+    #[builder(default = 0)]
+    pub playback_duration_percent: i32
 }
 
 impl Config {
+    pub fn buffer_size(&self) -> u32 { self.sample_rate * self.buffer_length_ms / 1000 }
+
     pub fn buffer_delay(&self) -> Duration {
-        Duration::from_secs_f64(self.buffer_size as f64 / self.sample_rate as f64)
+        Duration::from_secs_f64(self.buffer_size() as f64 / self.sample_rate as f64)
     }
 }
