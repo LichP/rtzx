485f6c93133d46aa4c7590ec0023bd222387937a
Phil Stewart <p.stewart@imperial.ac.uk>
2025-12-28 12:30:16 +0000

    tzx/tzx_data: Rename data module to tzx_data

--- src/tzx.rs
+++ src/tzx.rs
@@ -2,6 +2,7 @@ pub mod blocks;
 pub mod config;
 pub mod header;
 pub mod data;
+pub mod tzx_data;
 pub mod platform;
 pub mod playlist;
 pub mod recovery_enum;
@@ -9,7 +10,7 @@ pub mod waveforms;
 
 pub use config::Config;
 pub use header::Header;
-pub use data::TzxData;
+pub use tzx_data::TzxData;
 pub use platform::Platform;
 pub use playlist::Playlist;
 pub use recovery_enum::RecoveryEnum;
