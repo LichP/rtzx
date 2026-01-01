9900dc79f150f59955015f971bb142d5c41ea680
Phil Stewart <p.stewart@imperial.ac.uk>
2026-01-01 14:55:58 +0000

    tzx/waveforms/data_waveform: Add DataPulseIterator and refactor to use it; show pulse sample widths in display"

--- src/tzx/waveforms/data_waveform.rs
+++ src/tzx/waveforms/data_waveform.rs
@@ -15,45 +15,85 @@ use crate::tzx::{
     waveforms::{Pulse, Waveform},
 };
 
-#[allow(dead_code)]
 #[derive(Clone)]
-pub struct DataWaveform {
-    config: Arc<Config>,
+pub struct DataPulseIterator {
     length_pulse_zero: u16,
     length_pulse_one: u16,
     payload: DataPayload,
     start_pulse_high: bool,
     current_pulse: Pulse,
     current_pulse_index: usize,
+}
+
+impl DataPulseIterator {
+    pub fn new(config: Arc<Config>, length_pulse_zero: u16, length_pulse_one: u16, payload: DataPayload, start_pulse_high: bool) -> Self {
+        return Self {
+            length_pulse_zero,
+            length_pulse_one,
+            payload,
+            start_pulse_high,
+            current_pulse: Pulse::new(config, 0, false),
+            current_pulse_index: 0,
+        }
+    }
+}
+
+impl Iterator for DataPulseIterator {
+    type Item = Pulse;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_pulse_index < self.len() {
+            let current_bit = self.payload.data.view_bits::<Msb0>()[self.current_pulse_index as usize / 2];
+            self.current_pulse.length = if current_bit { self.length_pulse_one } else { self.length_pulse_zero };
+            self.current_pulse.high = !((self.current_pulse_index % 2 == 0) ^ self.start_pulse_high);
+            self.current_pulse_index += 1;
+            Some(self.current_pulse.clone())
+        } else {
+            None
+        }
+    }
+}
+
+impl DataPulseIterator {
+    fn len(&self) -> usize { self.payload.total_bits() * 2 }
+
+    fn prev(&mut self) -> Option<Pulse> {
+        if self.current_pulse_index > 0 {
+            self.current_pulse_index -= 1;
+            let current_bit = self.payload.data.view_bits::<Msb0>()[self.current_pulse_index as usize / 2];
+            self.current_pulse.length = if current_bit { self.length_pulse_one } else { self.length_pulse_zero };
+            self.current_pulse.high = !((self.current_pulse_index % 2 == 0) ^ self.start_pulse_high);
+            Some(self.current_pulse.clone())
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct DataWaveform {
+    config: Arc<Config>,
+    length_pulse_zero: u16,
+    length_pulse_one: u16,
+    payload: DataPayload,
+    pulse_iterator: DataPulseIterator,
+    current_pulse: Pulse,
     current_pulse_sample_index: usize,
 }
 
 impl DataWaveform {
     pub fn new(config: Arc<Config>, length_pulse_zero: u16, length_pulse_one: u16, payload: DataPayload, start_pulse_high: bool) -> Self {
-        let first_bit = payload.data.view_bits::<Msb0>()[0];
-        let first_bit_length = if first_bit { length_pulse_one } else { length_pulse_zero };
-
-        let current_pulse = Pulse::new(config.clone(), first_bit_length, start_pulse_high);
+        let mut pulse_iterator = DataPulseIterator::new(config.clone(), length_pulse_zero, length_pulse_one, payload.clone(), start_pulse_high);
+        let current_pulse = pulse_iterator.next().unwrap_or(Pulse::new(config.clone(), 0, false));
 
         return Self {
             config,
             length_pulse_zero,
             length_pulse_one,
-            payload: payload,
-            start_pulse_high,
+            payload,
+            pulse_iterator,
             current_pulse,
-            current_pulse_index: 0,
             current_pulse_sample_index: 0,
-        }
-    }
-
-    fn has_data_remaining(&self) -> bool { self.current_pulse_index < self.payload.total_bits() * 2 }
-
-    fn update_pulse(&mut self) {
-        if self.has_data_remaining() {
-            let current_bit = self.payload.data.view_bits::<Msb0>()[self.current_pulse_index as usize / 2];
-            self.current_pulse.length = if current_bit { self.length_pulse_one } else { self.length_pulse_zero };
-            self.current_pulse.high = !((self.current_pulse_index % 2 == 0) ^ self.start_pulse_high)
         }
     }
 }
@@ -61,11 +101,15 @@ impl DataWaveform {
 impl fmt::Display for DataWaveform {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let bit_counts = self.payload.bit_counts();
-        write!(f, "DataWaveform:   {:6} / {:6} pulses (0+1: {}+{})",
-            self.current_pulse_index,
+        write!(f, "DataWaveform:   {:6} / {:6} pulses (0+1: {}+{}; w0: {:.2}→{}; w1: {:.2}→{})",
+            self.pulse_iterator.current_pulse_index,
             bit_counts.total * 2,
             bit_counts.zeros,
             bit_counts.ones,
+            self.length_pulse_zero as f64 * self.config.platform.t_cycle_secs_playback(self.config.playback_duration_percent) * self.config.sample_rate as f64,
+            (self.length_pulse_zero as f64 * self.config.platform.t_cycle_secs_playback(self.config.playback_duration_percent) * self.config.sample_rate as f64).round() as u32,
+            self.length_pulse_one as f64 * self.config.platform.t_cycle_secs_playback(self.config.playback_duration_percent) * self.config.sample_rate as f64,
+            (self.length_pulse_one as f64 * self.config.platform.t_cycle_secs_playback(self.config.playback_duration_percent) * self.config.sample_rate as f64).round() as u32,
         )
     }
 }
@@ -74,20 +118,16 @@ impl Iterator for DataWaveform {
     type Item = f32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.current_pulse_index < self.payload.total_bits() * 2 {
-            let pulse_sample: Option<Self::Item> = self.current_pulse.get_next_sample(self.current_pulse_sample_index as u32);
-            if pulse_sample.is_some() {
-                self.current_pulse_sample_index += 1;
-                return pulse_sample;
-            }
-
-            self.current_pulse_index += 1;
+        let pulse_sample: Option<Self::Item> = self.current_pulse.get_next_sample(self.current_pulse_sample_index as u32);
+        if pulse_sample.is_some() {
+            self.current_pulse_sample_index += 1;
+            return pulse_sample;
+        }
+        let next_pulse = self.pulse_iterator.next();
+        if next_pulse.is_some() {
+            self.current_pulse = next_pulse.unwrap();
             self.current_pulse_sample_index = 0;
-
-            if self.has_data_remaining() {
-                self.update_pulse();
-                return self.next();
-            }
+            return self.next();
         }
         return None;
     }
@@ -126,22 +166,22 @@ impl Source for DataWaveform {
         pulse.length = self.length_pulse_zero;
         pulse_samples += pulse.len() as usize * bit_counts_to_index.zeros * 2;
 
-        self.current_pulse_index = estimated_byte_index * 16;
+        self.pulse_iterator.current_pulse_index = estimated_byte_index * 16;
         self.current_pulse_sample_index = 0;
 
-        while pulse_samples > samples && self.current_pulse_index > 0 {
+        while pulse_samples > samples && let Some(prev_pulse) = self.pulse_iterator.prev() {
+            self.current_pulse = prev_pulse;
             pulse_samples -= self.current_pulse.len() as usize;
-            self.current_pulse_index -= 1;
-            self.update_pulse();
         }
 
-        while pulse_samples < samples && self.has_data_remaining() {
+        while pulse_samples < samples {
             pulse_samples += self.current_pulse.len() as usize;
             if pulse_samples > samples {
                 self.current_pulse_sample_index = pulse_samples - samples;
             } else {
-                self.current_pulse_index += 1;
-                self.update_pulse();
+                if let Some(next_pulse) = self.pulse_iterator.next() {
+                    self.current_pulse = next_pulse;
+                }
             }
         }
         return Ok(());
@@ -153,11 +193,12 @@ impl Waveform for DataWaveform {
         Box::new(self.clone())
     }
 
-    fn started(&self) -> bool { self.current_pulse_index > 0 || self.current_pulse_sample_index > 0 }
+    fn started(&self) -> bool { self.pulse_iterator.current_pulse_index > 0 || self.current_pulse_sample_index > 0 }
 
     fn visualise(&self, pulse_string_length: usize) -> String {
         let mut pulse_string = "".to_string();
-        let mut pulse_index = self.current_pulse_index;
+
+        let mut pulse_index = self.pulse_iterator.current_pulse_index;
         let mut current_high = self.current_pulse.high;
         let mut current_char: char;
         while pulse_string.chars().count() < pulse_string_length && pulse_index < self.payload.total_bits() * 2 {
@@ -176,8 +217,8 @@ impl Waveform for DataWaveform {
         Some(
             DataPayloadWithPosition {
                 payload: self.payload.clone(),
-                current_byte_index: self.current_pulse_index as usize / 16,
-                current_bit_index: ((self.current_pulse_index / 2) % 8 ) as u8,
+                current_byte_index: self.pulse_iterator.current_pulse_index as usize / 16,
+                current_bit_index: ((self.pulse_iterator.current_pulse_index / 2) % 8 ) as u8,
             }
         )
     }
