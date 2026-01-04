1870d51d9cc0b576e5f08856b67dcbd282a5fafa
Phil Stewart <p.stewart@imperial.ac.uk>
2026-01-04 15:51:37 +0000

    tzx/header: Implement traits and add constructor

--- src/tzx/header.rs
+++ src/tzx/header.rs
@@ -5,9 +5,18 @@ use std::fmt;
 
 #[binrw]
 #[brw(little, magic = b"ZXTape!\x1A")]
+#[derive(Clone, Debug)]
 pub struct Header {
     major: u8,
     minor: u8
+}
+
+impl Header {
+    pub fn new(major: u8, minor: u8) -> Self { Header { major, minor }}
+}
+
+impl Default for Header {
+    fn default() -> Self { Header::new(1, 20) }
 }
 
 impl fmt::Display for Header {
